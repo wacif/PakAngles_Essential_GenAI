b7146bc0fa21567b4c5ff35200dcc4810e8b4650
Wasif Nawaz <nwasi1930@gmail.com>
2024-08-26 12:16:32 +0500

    testing an error

---
+++ test_folder/simple_chatbot/01.py
@@ -0,0 +1,29 @@
+x=5
+y="John"
+print(x)
+print(y)
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+fruits = ["apple" , "banana" , "cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
+x = "Python"
+y = "is"
+z = "awesome"
+print(x, y, z)
+x = "awesome"
+
+def myfunc():
+    print("Python is " + x)
+
+myfunc()
